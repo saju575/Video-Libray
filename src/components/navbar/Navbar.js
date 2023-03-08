26b2354208a72f5a1d63205710606c7411ffa6c1
saju575 <sakilahmedsaju575@gmail.com>
2023-03-08 20:20:21 +0600

    Converted html to react UI

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Search from "./Search";
+import imgSearch from "../../assets/search.svg";
+import logoImage from "../../assets/videoLogo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-100 shadow-md">
+      <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
+        <a href="/">
+          <img className="h-10" src={logoImage} alt="Learn with Sumit" />
+        </a>
+        <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
+          {/* <!-- search --> */}
+          <Search />
+          <img
+            className="inline h-4 cursor-pointer"
+            src={imgSearch}
+            alt="Search"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
